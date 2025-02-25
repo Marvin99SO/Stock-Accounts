93e5b7490f81c04598c9834088ed1343814b319d
Marcus Vinicius Santos de Oliveira <marcusvsdo@gmail.com>
2025-02-25 17:32:29 -0300

    init: estrutura inicial do projeto Spring Boot

---
+++ src/test/java/com/example/stockaccounts/StockaccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockaccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockaccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
