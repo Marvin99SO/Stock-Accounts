f75a679c13876bdf65a97125238a7ca90a5067ef
Marcus Vinicius Santos de Oliveira <marcusvsdo@gmail.com>
2025-02-25 19:03:16 -0300

    feat: adiciona a entidade Usuário/UserEntity

---
+++ src/main/java/com/example/stockaccounts/entity/UserEntity.java
@@ -0,0 +1,39 @@
+package com.example.stockaccounts.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID userId;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    private Instant creationTimeStamp;
+    @UpdateTimestamp
+    private Instant updatedTimeStamp;
+
+}
