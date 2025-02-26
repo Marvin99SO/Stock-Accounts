f38809ceec4da1da9eeeda6bb4d742dddb4b0346
Marcus Vinicius Santos de Oliveira <marcusvsdo@gmail.com>
2025-02-26 19:48:57 -0300

    refactor: descontinuação das anotações do lombok

--- src/main/java/com/example/stockaccounts/entity/User.java
+++ src/main/java/com/example/stockaccounts/entity/User.java
@@ -1,10 +1,6 @@
 package com.example.stockaccounts.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -13,10 +9,6 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "tb_users")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
@@ -36,4 +28,64 @@ public class User {
     @UpdateTimestamp
     private Instant updatedTimeStamp;
 
+    public User() {
+    }
+
+    //public User(UUID userId, String username, String email, String password, Instant creationTimeStamp, Instant updatedTimeStamp) {
+    public User(String username, String email, String password, Instant creationTimeStamp, Instant updatedTimeStamp) {
+        //this.userId = userId;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.creationTimeStamp = creationTimeStamp;
+        this.updatedTimeStamp = updatedTimeStamp;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Instant getCreationTimeStamp() {
+        return creationTimeStamp;
+    }
+
+    public void setCreationTimeStamp(Instant creationTimeStamp) {
+        this.creationTimeStamp = creationTimeStamp;
+    }
+
+    public Instant getUpdatedTimeStamp() {
+        return updatedTimeStamp;
+    }
+
+    public void setUpdatedTimeStamp(Instant updatedTimeStamp) {
+        this.updatedTimeStamp = updatedTimeStamp;
+    }
 }
