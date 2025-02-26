72858865087266806a7fbfeaa788a02311a41e56
Marcus Vinicius Santos de Oliveira <marcusvsdo@gmail.com>
2025-02-26 19:52:24 -0300

    feat: adiciona a interface UserRepository

---
+++ src/main/java/com/example/stockaccounts/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.stockaccounts.repository;
+
+import com.example.stockaccounts.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
